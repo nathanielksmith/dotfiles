dbf38385f356b763bd44334f10a52ac72f9b1813
nathaniel smith <nathanielksmith@gmail.com>
2013-01-22 19:59:47 -0500

    lightm > slim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get install python-pip vim zsh tmux build-essential stalonetray wicd wicd-curses xmonad lxterminal lightdm w3m chromium sudo suckless-tools
+apt-get install python-pip vim zsh tmux build-essential stalonetray wicd wicd-curses xmonad lxterminal slim w3m chromium sudo suckless-tools
 
 pip install sh
