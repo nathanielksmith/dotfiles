893cabbb3d1bd93d7de4171951990597346b422f
nathaniel smith <nathanielksmith@gmail.com>
2014-11-08 18:07:30 -0500

    add xmonad+tint2

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,12 @@
 import XMonad
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 
-main = do
-    spawn "gnome-settings-daemon"
-    xmonad $ defaultConfig {
-        modMask = mod4Mask,
-        borderWidth = 4,
-        normalBorderColor = "#3A1957",
-        focusedBorderColor = "#535719"
-        } `additionalKeys`
-        [ ((mod4Mask, xK_r), spawn "dmenu_run") ]
+main :: IO ()
+main = do 
+  xmonad $ ewmh defaultConfig 
+    { modMask = mod4Mask
+    , manageHook = manageHook defaultConfig <+> manageDocks
+    , handleEventHook = handleEventHook defaultConfig <+> docksEventHook <+> fullscreenEventHook
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    }
